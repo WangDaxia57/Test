060a737c4c23deee1ad7a71f88e362d4b3636967
WangDaxia57 <wang987662210@163.com>
2017-11-08 14:49:14 +0800

    xmlTool

---
+++ XML/XmlUtils.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.IO;
+using UnityEngine;
+
+namespace MonitorTool.src.core
+{
+    class XmlUtils
+    {
+
+        ///<summary>
+        /// 获的路径下的所有.xml文件
+        ///</summary>
+        ///<param name="xmlFileName">XML文档完全文件名(包含物理路径)</param>
+        ///<returns>返回Dictionary</returns>
+        public static string[] getXmlFileList(string path)
+        {
+            return Directory.GetFiles(path, "*.xml");
+        }
+
+        ///<summary>
+        /// 获得解析好的XML
+        ///</summary>
+        ///<param name="xmlFileName">XML文档完全文件名(包含物理路径)</param>
+        ///<returns>返回Dictionary</returns>
+        public static Dictionary<String, Dictionary<String, String>> getXmlList(string xmlFileName)
+        {
+            Dictionary<String, Dictionary<String, String>> xmlList = new Dictionary<String, Dictionary<String, String>>();
+         Dictionary<string,string> dic=new Dictionary<string, string>();
+            XmlDocument xmlDoc = new XmlDocument();
+            try
+            {
+                xmlDoc.Load(xmlFileName); //加载XML文档
+                //取到所有的xml结点
+                XmlNodeList nodelist = xmlDoc.GetElementsByTagName("*");
+                for (int i = 1; i < nodelist.Count; ++i)
+                {
+                    XmlNode xmlNode = nodelist.Item(i);
+                    string a = xmlNode.OuterXml;
+                    XmlAttributeCollection xmlListstr = xmlNode.Attributes;
+                    Dictionary<String, String> xmlListline = new Dictionary<String, String>();
+                    foreach (XmlAttribute item in xmlListstr)
+                    {
+                        string Name = item.Name;
+                        string InnerText = item.InnerText;
+                        xmlListline.Add(Name, InnerText);
+                    }
+                    //当前Map存在数据
+                    if (xmlListline.Count > 0)
+                    {
+                        String value = "";
+                        xmlListline.TryGetValue(xmlListline.Keys.First(), out value);
+                        if (!"".Equals(value))
+                        {
+                            xmlList.Add(value, xmlListline);
+                        }
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.Log(xmlFileName);
+                return null;
+                //throw ex; //这里可以定义你自己的异常处理
+            }
+            return xmlList;
+        }
+
+        /// <summary>
+        /// 修改单行数据
+        /// </summary>
+        /// <param name="xmlFileName">XML地址</param>
+        /// <param name="MapTable">单行数据。第一项必须是唯一键值对</param>
+        public static void Modify(string xmlFileName, Dictionary<string, string> MapTable)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            try
+            {
+                xmlDoc.Load(xmlFileName); //加载XML文档
+                XmlElement root = xmlDoc.DocumentElement;   //获取根节点 
+                //取到所有的xml结点
+                XmlNodeList nodelist = xmlDoc.GetElementsByTagName("*");
+                foreach (XmlNode node in nodelist)
+                {
+                    XmlNode xmlNode = node;
+                    XmlElement ele = (XmlElement)node;
+                    if (ele.GetAttribute(MapTable.Keys.First()) == MapTable[MapTable.Keys.First()])
+                    {
+                        foreach (string item in MapTable.Keys)
+                        {
+                            if (ele.GetAttribute(item) != MapTable[item])
+                            {
+                                ele.SetAttribute(item, MapTable[item]);
+                            }
+                        }
+                    }
+                }
+               Debug.Log("节点修改成功");
+               xmlDoc.Save(xmlFileName);
+            }
+            catch (System.Exception ex)
+            {
+                Debug.Log(xmlFileName + ex);
+                return;
+            }
+        }
+        /// <summary>
+        /// 修改单行单列上的一个数据
+        /// </summary>
+        /// <param name="xmlFileName">要修改xml地址</param>
+        /// <param name="CheckKey">唯一key</param>
+        /// <param name="CheckValue">唯一值</param>
+        /// <param name="key">要修改的key</param>
+        /// <param name="value">要修改的值</param>
+        public static void Modify(string xmlFileName, string CheckKey, string CheckValue, string key, string value)
+        {
+             XmlDocument xmlDoc = new XmlDocument();
+             try
+             {
+                 xmlDoc.Load(xmlFileName); //加载XML文档
+                 XmlElement root = xmlDoc.DocumentElement;   //获取根节点 
+                 //取到所有的xml结点
+                 XmlNodeList nodelist = xmlDoc.GetElementsByTagName("*");
+                 foreach (XmlNode node in nodelist)
+                 {
+                    XmlElement ele = (XmlElement)node;
+                    if (ele.GetAttribute(CheckKey) == CheckValue)
+                    {
+                        if (ele.GetAttribute(key) != value)
+                        {
+                            ele.SetAttribute(key, value);
+                        }
+                    }
+                 }
+             }
+             catch (System.Exception ex)
+             {
+                 Debug.Log(xmlFileName);
+                 return;
+             }
+        }
+
+
+    }
+}
---
+++ XML/XmlUtilsMgr.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//xml管理类
+namespace MonitorTool.src.core
+{
+    using UnityEngine; //所有表的缓存
+    using DicMapTable = Dictionary<String, Dictionary<String, String>>;  //一级枚举,一张表的数据
+    using DicMapTableAll = Dictionary<String, Dictionary<String, Dictionary<String, String>>>;
+
+    class XmlUtilsMgr
+    {
+        private static XmlUtilsMgr xmlUtilsMgr;
+        private static bool m_init = false;
+        //获得管理类
+        public static XmlUtilsMgr newInstance(){
+            if (xmlUtilsMgr == null)
+            {
+                xmlUtilsMgr = new XmlUtilsMgr();
+                xmlUtilsMgr.Init();
+            }
+            return xmlUtilsMgr;
+        }
+
+        private DicMapTableAll configCache;
+        private string defPath = null;
+        //缓存所有表数据
+        public void Init()
+        {
+            if (m_init)
+                return;
+            // 初始化表格
+            configCache = new DicMapTableAll();
+
+            defPath = GlobalConfig.Instance.ConfigPath.Replace("\\", "/");
+            String[] fileList = XmlUtils.getXmlFileList(defPath);
+            foreach (String fileName in fileList)
+            {
+               string fname = fileName.Replace("\\", "/");
+               DicMapTable map = XmlUtils.getXmlList(fname);
+               configCache.Add(fname, map);
+            }
+            m_init = true;
+        }
+
+        //直接获取单行数据某个key的值
+        public String getItemKey(String fileName, String key, String keys)
+        {
+            if (configCache == null)
+            {
+                return null;
+            }
+            Dictionary<String, String> map = getItem(fileName, key);
+            String value = null;
+            Boolean isRead = map.TryGetValue(keys, out value);
+            return value;
+        }
+
+        //获得单行数据
+        public Dictionary<String, String> getItem(String fileName, String key)
+        {
+            if (fileName.Length < defPath.Length)
+                fileName = string.Format("{0}{1}", defPath, fileName + ".xml");
+            Dictionary<String, String> map = new Dictionary<String, String>();
+            if (configCache == null)
+            {
+                return null;
+            }
+            DicMapTable dicMap;
+            Boolean isRead = configCache.TryGetValue(fileName, out dicMap);
+            if (isRead)
+            {
+                dicMap.TryGetValue(key, out map);
+            }
+            else
+            {
+                return null;
+            }
+
+            return map;
+        }
+
+        //获得整个表的数据
+        public DicMapTable getAll(String fileName)
+        {
+            if (fileName.Length < defPath.Length)
+                fileName = string.Format("{0}{1}", defPath, fileName + ".xml");
+            if (configCache == null)
+            {
+                return null;
+            }
+            DicMapTable dicMap;
+            Boolean isRead = configCache.TryGetValue(fileName, out dicMap);
+            if (isRead)
+            {
+                return dicMap;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        
+    }
+}
